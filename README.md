6e17d049bd22b506f00e3d50f380e9344cfdeeaa
Arnaud J <arnaudj@users.noreply.github.com>
2018-10-28 23:50:41 +0100

    Introduce react hooks (16.7.0-alpha.0)
    And misc integration with mobx-react

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # HN React client
+
+## Done
+- Experiment with react hooks
+
+## TODOs
+- Material design
+- API: https://hn.algolia.com/api
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import ReactDOM from "react-dom";
-import React from "react";
+import React, { useEffect } from "react";
 import { observable, action, configure, runInAction, autorun } from "mobx";
 import { observer, inject, Provider } from "mobx-react";
 import DevTools from "mobx-react-devtools";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { apiGetComments } from "../api";
+
+/**
+ * React hooks and mobx-react: mobx-react isn't ready for hooks;
+ * -: requires a workaround: use a dedicated function component, as hooks do not support classes and inject/observe on a function do not match hooks invariant check 'Hooks can only be called inside the body of a function component'
+ * +: component better isolated (but still staful due to useEffect)
+ */
 
 configure({ enforceActions: "always" });
 
@@ -14,62 +20,61 @@ const Home = () => (
   </div>
 );
 
-@inject("store")
-@observer
-class StoryComponent extends React.Component {
-  // lifecycle: everytime displayed by router (even on visit again)
-  componentWillMount() {
-    const {
-      store,
-      match: {
-        params: { storyId }
-      }
-    } = this.props;
-    store.actionUserNavigatesToStory(storyId);
-  }
+// use function component for useEffect
+const StoryComponentFunctionComponent = ({ onShow, story, comments }) => {
+  useEffect(() => {
+    onShow();
+  });
 
-  // lifecycle: everytime navigated away by router
-  componentWillUnmount() {}
-
-  render() {
-    const {
-      store,
-      match: {
-        params: { storyId }
-      }
-    } = this.props;
-
-    console.log(`story#${storyId}: render()`);
-
-    const story = store.stories.get(Number(storyId));
-    const storyComments = story.comments;
-    const hasComments = storyComments.length; // hit array property now to register observer atom (since story.isFetching eq true at first run, checking comments length later after isFetching check is too late and wouldn't trigger a re-render otherwise)
-    return (
-      <div>
-        <h3>
-          Reading story {storyId}: {story.title}
+  return (
+    <div>
+      <h3>
+        Reading story {story.id}: {story.title}
+        <br />
+      </h3>
+      {story.isFetching ? (
+        <span>Loading...</span>
+      ) : (
+        <div>
+          Story comments:
           <br />
-        </h3>
-        {story.isFetching ? (
-          <span>Loading...</span>
-        ) : (
-          <div>
-            Story comments:
-            <br />
-            {hasComments
-              ? storyComments.map(comment => (
-                  <div key={comment.id}>
-                    - {comment.author}: {comment.comment}
-                    <br />
-                  </div>
-                ))
-              : "No comment"}
-          </div>
-        )}
-      </div>
+          {comments.length
+            ? comments.map(comment => (
+                <div key={comment.id}>
+                  - {comment.author}: {comment.comment}
+                  <br />
+                </div>
+              ))
+            : "No comment"}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const StoryComponent = inject("store")(
+  observer(function doit(props) {
+    const {
+      store,
+      match: {
+        params: { storyId }
+      }
+    } = props;
+
+    const story = store.getStory(storyId);
+    // hit array property now to register observer atom (within render's immediate body and don't hide behind isLoading either)
+    // Because observer only applies to exactly the render function of the current component; passing a render callback or component to a child component doesn't become reactive automatically
+    // https://github.com/mobxjs/mobx/blob/gh-pages/docs/best/react.md#mobx-only-tracks-data-accessed-for-observer-components-if-they-are-directly-accessed-by-render
+    const hasComments = story.comments.length;
+    return (
+      <StoryComponentFunctionComponent
+        onShow={() => store.actionUserNavigatesToStory(story.id)}
+        story={story}
+        comments={story.comments}
+      />
     );
-  }
-}
+  })
+);
 
 const StoriesList = inject("store")(({ baseUrl, store: { stories } }) => (
   <div>
@@ -135,7 +140,10 @@ class Store {
 
   constructor() {
     autorun(() => {
-      if (this._fetchList.length === 0) return; // touch fetchList property to register reaction
+      if (this._fetchList.length === 0) {  // touch fetchList property to register reaction
+        console.log(`Store.Fetch fetchList: empty}`);
+        return;
+      }
       const storyId = this.pollFetchList();
 
       if (this.getStory(storyId).comments.length) {
@@ -171,6 +179,8 @@ class Store {
 
   @action.bound
   actionUserNavigatesToStory(id) {
+    console.log(
+      "actionUserNavigatesToStory(): go");
     this._fetchList.push(Number(id));
     console.log(
       "actionUserNavigatesToStory(): after: _fetchList: " + this._fetchList
@@ -183,7 +193,7 @@ class Store {
   }
 
   getStory(storyId) {
-    return this.stories.get(storyId);
+    return this.stories.get(Number(storyId));
   }
 
   @action.bound
@@ -192,31 +202,25 @@ class Store {
   }
 }
 
-class BasicExample extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { store: props.store }; // hop via state, else in render() propagation of props to Stories will fail on 2nd iteration (happens right after 1st one where props is available)
-  }
-  render() {
-    return (
-      <Provider store={this.state.store}>
-        <React.Fragment>
-          <Router>
-            <div>
-              <Link to="/">Home</Link>
-              <br />
-              <Link to="/stories">Stories</Link>
-              <hr />
-              <Route exact path="/" component={Home} />
-              <Route path="/stories" render={props => <Stories {...props} />} />
-            </div>
-          </Router>
-          <DevTools />
-        </React.Fragment>
-      </Provider>
-    );
-  }
-}
+const BasicExample = props => {
+  return (
+    <Provider store={props.store}>
+      <React.Fragment>
+        <Router>
+          <div>
+            <Link to="/">Home</Link>
+            <br />
+            <Link to="/stories">Stories</Link>
+            <hr />
+            <Route exact path="/" component={Home} />
+            <Route path="/stories" render={props => <Stories {...props} />} />
+          </div>
+        </Router>
+        <DevTools />
+      </React.Fragment>
+    </Provider>
+  );
+};
 
 const store = new Store();
 store.initStore();
